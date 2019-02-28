9c5770b54c03d664a2a54c0528feb1210c7854fe
Thilo <login@thilo-langbein.de>
2019-02-28 22:22:16 +0100

    Update Check-Uri.ps1

--- PowerShellBrokenLinkFinder/Private/Check-Uri.ps1
+++ PowerShellBrokenLinkFinder/Private/Check-Uri.ps1
@@ -1,13 +1,25 @@
-function Check-Uri([string]$Uri)
-{
+function Check-Uri([string]$Uri) {
+    $Static_Extensions = @("jpg", "pdf")
+    [bool]$Is_Static = $false;
+
+    $Uri = $Uri.Trim().ToLower()
+
     Write-Verbose "Check-Uri '$Uri'"
 
-    try 
-    {
-        Invoke-WebRequest -Uri $Uri -ErrorAction Stop | Select-Object -ExpandProperty StatusCode    
+    foreach ($s in $Static_Extensions) {
+        if ($uri.EndsWith($s)) { $Is_Static = $true}
     }
-    catch [System.Net.WebException] 
-    {
+
+    try {
+        if ($Is_Static) {
+            Invoke-WebRequest -Uri $Uri -Method Head -ErrorAction Stop | Select-Object -ExpandProperty StatusCode    
+        }
+        else {
+            Invoke-WebRequest -Uri $Uri -Method Get -ErrorAction Stop | Select-Object -ExpandProperty StatusCode        
+        }
+        
+    }
+    catch [System.Net.WebException] {
         500
     }
 }
